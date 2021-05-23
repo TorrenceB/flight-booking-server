cfbc97f395246c11df75a0da6501de8fd819acda
Torrence Brown <torrenceb90@gmail.com>
2021-05-23 11:27:36 -0700

    Initial Commit

---
+++ server.js
@@ -0,0 +1,9 @@
+/*  1. Initialize npm and install dependencies:
+        - Nodemon
+        - Express.js
+    2. Initialize git repo
+    3. Initialize server, assign port number
+    4. React UI calls express server rather than SkyScanner
+    5. Parameters are forwarded as they are
+    6. Deliver SkyScanner response to UI
+*/
