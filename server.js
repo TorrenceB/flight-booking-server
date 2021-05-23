22e3676c2a4db96c4620c7705e21bd4abb8f670c
Torrence Brown <torrenceb90@gmail.com>
2021-05-23 11:56:25 -0700

    Created node server

--- server.js
+++ server.js
@@ -7,3 +7,13 @@
     5. Parameters are forwarded as they are
     6. Deliver SkyScanner response to UI
 */
+
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("<h1>Flight Booking API</h1>");
+});
+
+app.listen(port);
