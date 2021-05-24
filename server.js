ce83103142ba5ca5e37d9e084aa9351410a96f85
Torrence Brown <torrenceb90@gmail.com>
2021-05-23 17:34:37 -0700

    Wrote function to consume skyscanner

--- server.js
+++ server.js
@@ -12,8 +12,35 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+const axios = require("axios").default;
+
+const fetchFlightData = () => {
+  const options = {
+    method: "GET",
+    url: "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/autosuggest/v1.0/UK/GBP/en-GB/",
+    params: { query: "Stockholm" },
+    headers: {
+      "x-rapidapi-key": "069dfdaf8bmshbf72b4525dae034p10dc6ejsn27b330717476",
+      "x-rapidapi-host":
+        "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
+    },
+  };
+
+  const apiInstance = axios
+    .request(options)
+    .then((res) => {
+      console.log(res.data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+
+  return apiInstance;
+};
 app.get("/", (req, res) => {
-  res.send("<h1>Flight Booking API</h1>");
+  res.send("A response from the server");
+  fetchFlightData();
 });
 
 app.listen(port);
+console.log(`App running at port ${port}`);
